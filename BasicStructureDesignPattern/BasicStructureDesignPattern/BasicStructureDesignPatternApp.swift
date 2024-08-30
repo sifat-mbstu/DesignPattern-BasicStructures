8a7ac4f397b103d6e51b542d8a66aa3163656679
Sifatul Islam <sifat.acc@gmail.com>
2024-08-30 09:02:36 +0600

    init commit

---
+++ BasicStructureDesignPattern/BasicStructureDesignPattern/BasicStructureDesignPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicStructureDesignPatternApp.swift
+//  BasicStructureDesignPattern
+//
+//  Created by Sifatul Islam on 30/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicStructureDesignPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicStructureDesignPattern/BasicStructureDesignPattern/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasicStructureDesignPattern
+//
+//  Created by Sifatul Islam on 30/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
